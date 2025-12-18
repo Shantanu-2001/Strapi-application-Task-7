696c711f353681b0e7d9ad6a8aa5ba936bbbe880
Shantanurana <78650181+Shantanu-2001@users.noreply.github.com>
2025-12-18 14:53:24 +0530

    Create rds.tf

---
+++ terraform/rds.tf
@@ -0,0 +1,31 @@
+resource "aws_db_subnet_group" "strapi" {
+  name       = "shantanu-strapi-db-subnet-group"
+  subnet_ids = data.aws_subnets.default.ids
+}
+
+resource "aws_security_group" "rds_sg" {
+  name   = "shantanu-strapi-rds-sg"
+  vpc_id = data.aws_vpc.default.id
+}
+
+resource "aws_security_group_rule" "ecs_to_rds" {
+  type                     = "ingress"
+  from_port                = 5432
+  to_port                  = 5432
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.rds_sg.id
+  source_security_group_id = aws_security_group.ecs_sg.id
+}
+
+resource "aws_db_instance" "strapi" {
+  identifier             = "strapi-db"
+  engine                 = "postgres"
+  instance_class         = "db.t3.micro"
+  allocated_storage      = 20
+  username               = "strapi"
+  password               = "strapi123"
+  db_name                = "strapi_db"
+  skip_final_snapshot    = true
+  db_subnet_group_name   = aws_db_subnet_group.strapi.name
+  vpc_security_group_ids = [aws_security_group.rds_sg.id]
+}
