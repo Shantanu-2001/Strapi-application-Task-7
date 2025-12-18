134b5fdefd22c0c5e63f9b8ebd80a368a3e2febe
Shantanurana <78650181+Shantanu-2001@users.noreply.github.com>
2025-12-18 14:51:16 +0530

    Create ecs.tf

---
+++ terraform/ecs.tf
@@ -0,0 +1,69 @@
+resource "aws_ecs_cluster" "strapi" {
+  name = "strapi-cluster-shantanu"
+
+  setting {
+    name  = "containerInsights"
+    value = "enabled"
+  }
+}
+
+resource "aws_ecs_task_definition" "strapi" {
+  family                   = "strapi-task"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = "512"
+  memory                   = "1024"
+
+  execution_role_arn = aws_iam_role.ecs_task_execution_role.arn
+  task_role_arn      = aws_iam_role.ecs_task_role.arn
+
+  container_definitions = jsonencode([
+    {
+      name  = "strapi"
+      image = var.docker_image
+
+      portMappings = [{
+        containerPort = 1337
+        protocol      = "tcp"
+      }]
+
+      environment = [
+        { name = "NODE_ENV", value = "production" },
+        { name = "HOST", value = "0.0.0.0" },
+        { name = "PORT", value = "1337" },
+
+        { name = "DATABASE_CLIENT", value = "postgres" },
+        { name = "DATABASE_HOST", value = aws_db_instance.strapi.address },
+        { name = "DATABASE_PORT", value = "5432" },
+        { name = "DATABASE_NAME", value = "strapi_db" },
+        { name = "DATABASE_USERNAME", value = "strapi" },
+        { name = "DATABASE_PASSWORD", value = "strapi123" }
+      ]
+
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          awslogs-group         = aws_cloudwatch_log_group.strapi.name
+          awslogs-region        = var.aws_region
+          awslogs-stream-prefix = "strapi"
+        }
+      }
+    }
+  ])
+}
+
+resource "aws_ecs_service" "strapi" {
+  name            = "strapi-service"
+  cluster         = aws_ecs_cluster.strapi.id
+  task_definition = aws_ecs_task_definition.strapi.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets          = data.aws_subnets.default.ids
+    security_groups  = [aws_security_group.ecs_sg.id]
+    assign_public_ip = true
+  }
+
+  depends_on = [aws_db_instance.strapi]
+}
